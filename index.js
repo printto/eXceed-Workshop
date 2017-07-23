142c3b32aa291a1662b8a5375283e793dafadad7
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2017-07-23 15:47:39 +0700

    Finalize 1

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ $(document).ready(function() {
 
   var link = "http://158.108.165.223/data/group13/"
 
-  var lastMessage = ""
+  var lightOn = false
+  var doorOn = false
+  var airOn = false
 
   <!-- people -->
   setInterval(function() {
     $.ajax({
-      url: link + "p"
+      url: link + "3"
     }).done(function(data) {
-      if (lastMessage != data) {
-        $('#people').text(data);
-        lastMessage = data
+      $('#people').text(data);
+      if (data == 0) {
+        $('#house_pic').attr("src", "img/home2.png");
+      } else {
+        $('#house_pic').attr("src", "img/home.png");
       }
     }).fail(function(data) {
       console.log(data);
@@ -21,12 +25,9 @@ $(document).ready(function() {
   <!-- temp -->
   setInterval(function() {
     $.ajax({
-      url: link + "t"
+      url: link + "2"
     }).done(function(data) {
-      if (lastMessage != data) {
-        $('#temp').text(data);
-        lastMessage = data
-      }
+      $('#temp').text(data);
     }).fail(function(data) {
       console.log(data);
     });
@@ -35,24 +36,51 @@ $(document).ready(function() {
   <!-- bright -->
   setInterval(function() {
     $.ajax({
-      url: link + "b"
+      url: link + "1"
     }).done(function(data) {
-      if (lastMessage != data) {
-        $('#bright').text(data);
-        lastMessage = data
+      $('#bright').text(data);
+    }).fail(function(data) {
+      console.log(data);
+    });
+  }, 1000);
+
+  <!-- door -->
+  setInterval(function() {
+    $.ajax({
+      url: link + "4"
+    }).done(function(data) {
+      if(data == "1"){
+        doorOn = true
+      }
+      else {
+        doorOn = false
+      }
+      if (doorOn) {
+        $('#door_pic').attr("src", "img/door-on.png");
+      } else {
+        $('#door_pic').attr("src", "img/door-off.png");
       }
     }).fail(function(data) {
       console.log(data);
     });
   }, 1000);
 
-  $('#send').click(function() {
-    var message = $('#message').val();
-    console.log(message);
+  $('#door').click(function() {
+    var doorChange = 1
+    if(doorOn == true){
+      doorChange = 0
+    }
     $.ajax({
-      url: link + "set/" + message
+      url: link + "4/set/" + doorChange
     }).done(function() {
-      console.log("Done");
+      if (doorChange == 1) {
+        $('#door_pic').attr("src", "img/door-on.png");
+        console.log("Door Opened");
+      }
+      else {
+        $('#door_pic').attr("src", "img/door-off.png");
+        console.log("Door Closed");
+      }
     }).fail(function() {
       console.log("Failed");
     });
